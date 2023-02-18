0c6e9a3d79a68bccababdc242d2a25e8247dd64b
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-02-18 18:58:16 +0530

    Create Simple Array Sum.cpp

---
+++ Simple Array Sum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int simpleArraySum(vector<int> arr){
+    int sum = 0;
+    int size = arr.size();
+    for(int i = 0; i<size; i++){
+        sum = sum+arr[i];
+    }
+    return sum;
+}
+
+
+int main(){
+    vector<int> arr;
+    int n;
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+    cout << simpleArraySum(arr);
+}
